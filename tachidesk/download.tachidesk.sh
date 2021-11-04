09193fad84887bddb5d290ed71b62fe74a3f4044
Jonathan <jsavargas@gmail.com>
2021-11-03 23:39:57 -0300

    status_code == 404; exit

--- tachidesk/download.tachidesk.sh
+++ tachidesk/download.tachidesk.sh
@@ -12,11 +12,20 @@ FINISH=$3
 
 
 x=$START
+i=1
+
 while [ $x -le $FINISH ]
 do
   #echo "downloading: curl http://192.168.0.10:4567/api/v1/download/$ID/chapter/$x"
   status_code=$(curl --write-out %{http_code} --silent http://192.168.0.10:4567/api/v1/download/$ID/chapter/$x)
   echo "status_code: ${status_code}  == downloading: curl http://192.168.0.10:4567/api/v1/download/$ID/chapter/$x"
+
+	if [ $status_code == 404 ] ; then
+		i=$(( $i + 1 ))
+		if [ $i -ge 10 ]; then
+			break
+		fi
+	fi
 
   x=$(( $x + 1 ))
 done
